2fbc9abc81096f32b4b5271f1917135dfe33718f
Douglas Heriot <douglas@douglasheriot.com>
2013-01-02 01:52:28 +1100

    Initial commit

---
+++ autoGrowingExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  autoGrowingExample
+//
+//  Created by Scott O'Brien on 1/01/13.
+//  Copyright (c) 2013 Scott O'Brien. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ autoGrowingExample/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  autoGrowingExample
+//
+//  Created by Scott O'Brien on 1/01/13.
+//  Copyright (c) 2013 Scott O'Brien. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
